0b2f359a1f2400885b18a43f1de8e22045080132
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-08-05 11:38:58 +0800

    Implement US-01 through US-04: Comic shop catalog and shopping cart

---
+++ ConsoleApp1/WebApplication1/Models/DTOs/UpdateCartDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Models.DTOs
+{
+    public class UpdateCartDTO : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsoleApp1/WebApplication1/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Repositories
+{
+    public class ProductRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
